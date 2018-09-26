e05df72458cae3f0533d8d8caf70b04f6eb7a741
jhonllanes <jhonllanes@jhonllanes-HP-1000-Notebook-PC>
2018-09-26 16:01:02 -0500

    crear archivos

---
+++ VentasCapas1.1/src/ventascapas1/pkg1/VentasCapas11.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ventascapas1.pkg1;
+
+/**
+ *
+ * @author jhonllanes
+ */
+public class VentasCapas11 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
